d0272ab6bc07b06932d61bafe4e75f27e123356e
Leandro Arturi <lea.arturi@gmail.com>
2023-05-02 14:11:50 -0300

    Vercel

--- server/routes.py
+++ server/routes.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter, Body
-from fastapi.encoders import jsonable_encoder
-from server.model import NoteSchema
 
 router = APIRouter()
 
@@ -15,68 +13,9 @@ notes = {
     }
 }
 
-
 @router.get("/")
 def get_notes() -> dict:
     return {
         "data": notes
-    }
-
-@router.get("/{id}")
-async def get_note(id: str) -> dict:
-    if int(id) > len(notes):
-        return {
-            "error": "Invalid note ID"
-        }
-
-    for note in notes.keys():
-        if note == id:
-            return {
-                "data": notes[note]
-            }
-
-    return {
-        "Error": "Invalid ID"
-    }
-
-@router.post("/")
-def add_note(note: NoteSchema = Body(...)) -> dict:
-    notes[str(len(notes)+1)] = note.dict()
-
-    return {
-        "message": "Note added successfully"
-    }
-#
-@router.put("/{id}")
-def update_note(id: str, note: NoteSchema):
-    stored_note = notes[id]
-    if stored_note:
-        stored_note_model = NoteSchema(**stored_note)
-        update_data = note.dict(exclude_unset=True)
-        updated_note = stored_note_model.copy(update=update_data)
-        notes[id] = jsonable_encoder(updated_note)
-        return {
-            "message": "Note updated successfully"
-        }
-    return {
-        "error": "No such note exist"
-    }
-
-@router.delete("/{id}")
-def delete_note(id: str) -> dict:
-    if int(id) > len(notes):
-        return {
-            "error": "Invalid note ID"
-        }
-
-    for note in notes.keys():
-        if note == id:
-            del notes[note]
-            return {
-                "message": "Note deleted"
-            }
-
-    return {
-        "error": "Note with {} doesn't exist".format(id)
     }
 
